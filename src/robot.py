17c910d3881b2f6ca1e2b04f10fcdfe8f3269228
Jin Xu <xu.jin.smile@gmail.com>
2013-09-16 12:29:39 +0800

    添加发图片功能

--- src/robot.py
+++ src/robot.py
@@ -4,28 +4,38 @@ import random
 from tools import login
 from tools import greet
 from tools import monitor
+from tools import loadpic
 
 
 def run():
-    #登陆
     client = login.login()
    
     while True:
         now = time.strftime('%M%S',time.localtime(time.time()))
 
         #整点检查
-        if now == '4159':
+        if now == '2520':
             monitor_info = ""
+            #random.choice(range(3))
             monitor_info += monitor.monitor_http()
+            #monitor_info += monitor.monitor_temp()
+
             greeting = ""
             greeting += greet.hello()
+            myPic = loadpic.pic()
             
-            
-            content = '%s。%s' %(greeting, monitor_info)
+            random.choice(range(4)) and \
+            content = '%s' %(greeting) or \
+            content = '%s%s' %(greeting, monitor_info)
             try:
-                client.statuses.update.post(status=content) 
+                #随机选择发图或者不发图,1/4可能性发图
+                random.choice(range(4)) and \
+                client.statuses.update.post(status=content) or \
+                client.statuses.upload.post(status=content, pic = myPic)
+                
             except:
                 pass 
+            myPic.close()
             print "Send succesfully!"
             time.sleep(1)
 
--- src/tools/monitor.py
+++ src/tools/monitor.py
@@ -3,11 +3,13 @@ import urllib
 import urllib2
 import os
 
+#监视http链接
 def monitor_http():
 	try:
 		urllib2.urlopen('http://ginsmile.github.io')
-		return "http://ginsmile.github.io神经连接正常。"
+		return "http://ginsmile.github.io 神经连接正常。"
 	except:
 		return '错了~~您的阵地被中二君攻陷！'
 
-#print monitor_http()
+
+
